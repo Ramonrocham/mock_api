9ce3101227cdd351b9c9afaa7c075a7706c30460
ramonrocham <anacletorrocham@gmail.com>
2025-08-22 16:58:14 -0300

    Feat: Metodos para alterar password

--- MockDataStorage.php
+++ MockDataStorage.php
@@ -2,17 +2,37 @@
 
 class MockDataStorage{
     public static function newUser($username, $password, $fullName, $email, $number = null){
+        if(!$username || !$password || !$fullName || !$email){
+            return array(
+                "status" => "error",
+                "message" => "Missing required fields",
+                "code" => 400
+            );
+        }
+        $s_username = strip_tags($username);
+        $s_password = strip_tags($password);
+        $s_fullName = strip_tags($fullName);
+        $s_email = strip_tags($email);
+        $Isemail = filter_var($s_email, FILTER_VALIDATE_EMAIL);
+        $s_number = $number ? strip_tags($number) : null;
+        if (!$Isemail) {
+            return array(
+                "status" => "error",
+                "message" => "Invalid email format",
+                "code" => 400
+            );
+        }
         $usersJson = file_get_contents("json/user.json");
         $users = json_decode($usersJson, true) ?? [];
         $users[count($users)] = array(
-            'username' => $username,
-            'password' => $password,
+            'username' => $s_username,
+            'password' => $s_password,
             'id' => "userID#0".(count($users) + 1),
             'status' => "active",
-            'name' => $fullName,
+            'name' => $s_fullName,
             'created_at' => date("Y-m-d H:i:s"),
-            'email' => $email,
-            'number' => $number ?? ""
+            'email' => $s_email,
+            'number' => $s_number ?? ""
         );
         file_put_contents("json/user.json", json_encode($users, JSON_PRETTY_PRINT));
         return array(
@@ -96,6 +116,34 @@ class MockDataStorage{
                     'status' => "falied"
                 );
         file_put_contents("json/logLogin.json", json_encode($logLogin, JSON_PRETTY_PRINT));
+        return array(
+            "status" => "error",
+            "message" => "Invalid username or password",
+            "code" => 401
+        );
+    }
+
+    public static function setNewPassword($username, $password, $newPassword){
+        $usersJson = file_get_contents("json/user.json");
+        $users = json_decode($usersJson, true);
+        if (!$users) {
+            return array(
+                "status" => "error",
+                "message" => "No user found",
+                "code" => 404
+            );
+        }
+        foreach ($users as &$user) {
+            if ($user['username'] === $username && $user['password'] === $password) {
+                $user['password'] = $newPassword;
+                file_put_contents("json/user.json", json_encode($users, JSON_PRETTY_PRINT));
+                return array(
+                    "status" => "success",
+                    "message" => "Password updated successfully",
+                    "code" => 200
+                );
+            }
+        }
         return array(
             "status" => "error",
             "message" => "Invalid username or password",
